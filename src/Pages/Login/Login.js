316dae6625aac48ebf98fc830b16d6c9539e81c6
Sarun Seepomkun <sarunseepomkun@gmail.com>
2022-11-15 18:03:18 +0700

    Update login page
    Update login page

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,27 +1,73 @@
-import React from "react";
+import React, { useState, createRef } from "react";
+import { useNavigate } from "react-router-dom";
 import "./main.css";
 import "./responsive.css";
 import background from "../../Assets/images/background.jpg";
+import { Login as Login_API } from "../../API/Authentication/authen";
 
 const Login = () => {
+  let navigate = useNavigate();
+  const txtUsername = createRef();
+  const txtPassword = createRef();
+
+  const [isLoading, setIsLoading] = useState(false);
+
+   function Login_Click() {
+    setIsLoading(true);
+
+    //if (txtUsername.target.value !== "" && txtPassword.target.value !== "") {
+      // const result = await Login_API();
+
+      // if(result?.data?.token)
+      // {
+      //   //Passed
+      //    navigate("/Todolist");
+      // }
+      // else
+      // {
+      //   //Failed
+      // }
+
+      setInterval(() => {
+        navigate("/Todolist");
+      }, 2000);
+
+    //}
+  }
+
   return (
     <>
-      <div class="container">
-        <img src={background} class="left_login_background" alt="background" />
-        <div class="forms-container">
-          <div class="signin">
-            <form action="#" class="sign-in-form">
-              <h1 class="title">KRUNGSRI RECOVERY SYSTEM</h1>
-              <div class="input-field">
-                <i class="fas fa-user"></i>
-                <input type="text" placeholder="Username" />
+      <div className="container">
+        <img src={background} classNameName="left_login_background" alt="background" />
+        <div className="forms-container">
+          <div className="signin">
+            <form action="#" className="sign-in-form">
+              <h1 className="title">KRUNGSRI RECOVERY SYSTEM</h1>
+              <div className="input-field">
+                <i className="fas fa-user"></i>
+                <input type="text" placeholder="Username" ref={txtUsername} />
               </div>
-              <div class="input-field">
-                <i class="fas fa-lock"></i>
-                <input type="password" placeholder="Password" />
+              <div className="input-field">
+                <i className="fas fa-lock"></i>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  ref={txtPassword}
+                />
               </div>
-              <input type="button" value="Login" class="btn solid" />
-              <p class="social-text">
+              {isLoading === true ? (
+                <button className="btn solid btn-login-loading">
+                  <i className="fa fa-refresh fa-spin"></i> Loading
+                </button>
+              ) : (
+                <input
+                  type="button"
+                  value="Login"
+                  className="btn solid"
+                  onClick={() => Login_Click()}
+                />
+              )}
+              <p className="social-text">
                 © 2023 Bank of Ayudhya Public Company Limited.
               </p>
             </form>
